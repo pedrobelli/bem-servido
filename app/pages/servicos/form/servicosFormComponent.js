477d1257431b2d22fc415255ec34ecf108c1462c
Pedro Belli <pedro.belli2@gmail.com>
2016-04-06 21:07:30 -0300

    Template e component de servicos.

--- app/pages/servicos/form/servicosFormComponent.js
+++ app/pages/servicos/form/servicosFormComponent.js
@@ -1,56 +1,41 @@
-define(['ko', 'text!./clientesFormTemplate.html', 'bridge'],
+define(['ko', 'text!./servicosFormTemplate.html', 'bridge'],
 function(ko, template, bridge) {
 
   var viewModel = function(params) {
     var self = this;
 
-    var pageHeaderText = params.name == 'new' ? 'Novo Cliente' : 'Editar Cliente';
-    var CREATE_PATH = "/api/clientes";
-    var UPDATE_PATH = "/api/clientes/"+params.id;
+    var pageHeaderText = params.name == 'new' ? 'Novo Serviço' : 'Editar Serviço';
+    var CREATE_PATH = "/api/servicos";
+    var UPDATE_PATH = "/api/servicos/"+params.id;
 
     self.id = ko.observable(params.id);
     self.nome = ko.observable();
-    self.email = ko.observable();
-    self.telefone = ko.observable();
-    self.senha = ko.observable();
-    self.confirmSenha = ko.observable();
+    self.valor = ko.observable();
     self.pageMode = ko.observable(pageHeaderText);
 
     self.validForm = ko.pureComputed(function(){
       valid = !!self.nome();
-      valid = valid && !!self.email();
+      valid = valid && !!self.valor();
 
-      if (isEditMode()) {
-        valid = valid && !!self.senha();
-      } else {
-        valid = valid && (!!self.senha() && !!self.confirmSenha());
-      }
       return valid;
     });
 
     self.save = function(){
       var path = isEditMode() ? UPDATE_PATH : CREATE_PATH;
 
-      if (!isEditMode() && (self.senha() != self.confirmSenha())) {
-        console.log("Erro: Os campos de senha estão diferentes!");
-        return;
-      }
-
       bridge.post(path, generatePayload())
       .fail(function(context, errorMessage, serverError){
         console.log("Erros: ", context.errors);
       })
       .done(function(){
-        window.location.hash = "clientes"
+        window.location.hash = "servicos"
       });
     };
 
     var generatePayload = function(){
       var payload = {
         nome     : self.nome(),
-        email    : self.email(),
-        telefone : self.telefone(),
-        senha    : self.senha()
+        valor    : self.valor(),
       };
 
       if(isEditMode()) payload.id = params.id;
@@ -60,15 +45,13 @@ function(ko, template, bridge) {
 
     var init = function(){
       if (isEditMode()) {
-        bridge.get("/api/clientes/"+params.id)
+        bridge.get("/api/servicos/"+params.id)
         .then(function(response){
           if (!response)
             return;
 
-          self.nome(response.cliente.nome);
-          self.email(response.cliente.email);
-          self.telefone(response.cliente.telefone);
-          self.senha(response.cliente.senha);
+          self.nome(response.servico.nome);
+          self.valor(response.servico.valor);
         });
       }
     };
@@ -84,7 +67,7 @@ function(ko, template, bridge) {
     viewModel: viewModel,
     template: template,
     title: function(params) {
-      return "Clientes form"
+      return "Serviços form"
     }
   };
 });
---
+++ app/pages/servicos/servicosComponent.js
@@ -0,0 +1,46 @@
+define(['ko', 'text!./servicosTemplate.html', 'bridge'],
+function(ko, template, bridge) {
+
+  var viewModel = function(params) {
+    var self = this;
+
+    self.servicos = ko.observableArray([]);
+
+    self.exclude = function(servico){
+      bridge.del("/api/servicos/"+servico.id)
+      .then(function(response){
+        init();
+      });
+    };
+
+    var mapResponseToServicos = function(servicos){
+      if(!servicos) return self.servicos([]);
+      var servicos = servicos.map(function(servico){
+        return {
+          id    : servico.id,
+          nome  : servico.nome,
+          valor : servico.email
+        };
+      });
+
+      self.servicos(servicos);
+    };
+
+    var init = function(){
+      bridge.get("/api/servicos")
+      .then(function(response){
+        mapResponseToServicos(response.servicos);
+      });
+    };
+
+    init();
+  };
+
+  return {
+    viewModel: viewModel,
+    template: template,
+    title: function(params) {
+      return "Serviços"
+    }
+  };
+});
