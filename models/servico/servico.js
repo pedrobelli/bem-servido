b983591ebca9be954250f28ab9c2ac8f2048772e
Pedro Belli <pedro.belli2@gmail.com>
2016-04-14 22:47:07 -0300

    Component de busca para servico.

--- models/servico/servico.js
+++ models/servico/servico.js
@@ -16,6 +16,11 @@ module.exports = function(sequelize, DataTypes) {
 			All: function(t){
 				return this.findAll({transaction: t});
 			},
+			Search: function(t, query){
+				return this.findAll({ where: { descricao: {
+					$like: '%'+query+'%'
+				} }, transaction: t});
+			},
 			Get: function(t, id){
 				return this.find({ where: { id: id }, transaction: t });
 			},
