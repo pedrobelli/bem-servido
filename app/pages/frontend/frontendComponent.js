aaca2de65af839d04bda3ce2cc320fac1300c622
Pedro Belli <pedro.belli2@gmail.com>
2016-11-10 00:45:01 -0200

    teste

--- app/pages/frontend/frontendComponent.js
+++ app/pages/frontend/frontendComponent.js
@@ -6,10 +6,7 @@ function(ko, template, bridge) {
 
     var init = function(){
       $('select').material_select();
-      $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-      });
+      $('.datepicker').pickadate({});
     }
 
     init();
